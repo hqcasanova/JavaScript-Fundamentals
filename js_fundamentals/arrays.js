c39ff9387f080528c3ac4b1a9eeb5cace8d09ca8
hqcasanova <hqcasanova@users.noreply.github.com>
2014-09-09 17:41:33 -0400

    Arrays finished

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -5,6 +5,27 @@
 // Create an array to hold your top choices (colors, presidents, whatever).
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+function ordinal(number) {
+    switch (number) {
+    case 1:
+        return '1st';
+    case 2:
+        return '2nd';
+    case 3:
+        return '3rd';
+    default:
+        return number + 'th';
+    }
+}
 
+var topChoices = ["green", "no president", "whoever", "nothing in particular"],
+    length,
+    i;
+
+numChoices = topChoices.length;
+
+for (i = 0; i < numChoices; i += 1) {
+    console.log("My #" + ordinal(i + 1) + " choice is " + topChoices[i] + ".");
+} 
 
 
